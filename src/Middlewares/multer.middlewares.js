b51d082f65936ce5bd108605d99f1059a5ae866b
Mohammed Sahil <sahilindian33@gmail.com>
2024-02-25 14:15:41 +0530

    file handling and Now finally setup for the app is completed now

---
+++ src/Middlewares/multer.middlewares.js
@@ -0,0 +1,17 @@
+// Creating a multer middleware
+
+// Multer is a library that is being used for the file  uploads in this
+
+import multer from "multer";
+
+// Storing the file in the local disk storage using the multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, ".../public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage });
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from "cloudinary";
+// Package for handling the file system or directory system
+import fs from "fs";
+
+// Configuring the cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Method to uploading a file on the cloudinary
+const uploadToCloud = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+      //   return "File Uploading failed";
+    }
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto", //This automatically detects the type of the file that is being uploaded
+    });
+
+    // file has been uploaded succesfully now
+    console.log("File is uplaoded on the cloudinary", response.url);
+    return response;
+
+  } catch (error) {
+    // This unlinks or deletes the file from the localally stored server when the upload to the cloud operation is failed 
+    fs.unlinkSync(localFilePath);
+    console.log("Error in File Upload : ", error);
+    return null;
+  }
+};
+
+export { uploadToCloud };
