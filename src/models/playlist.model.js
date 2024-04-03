74f19c75415c068fbc38d87a7388c028b6b7a2a0
Mohammed Sahil <sahilindian33@gmail.com>
2024-04-03 15:35:47 +0530

    created playlist,comment,likes,tweets models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Playlist = new mongoose.model("Playlist", playlistSchema);
+
+// Get all playlists
